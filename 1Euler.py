c1d37de3ed0862fa58c39ddd095b411d2a60390f
GlebLy <31567935+GlebLy@users.noreply.github.com>
2020-09-11 22:48:28 +0300

    Update 1Euler.py
    rabotaet

--- 1Euler.py
+++ 1Euler.py
@@ -1,7 +1,9 @@
 i = 2
-n = 10
+print("Enter number: ")
+n = int(input())
 sum = 0
 while i < n :
-    #if i % 3 == 0 and i % 5 == 0 or i % 3 == 0 or i % 5 ==0: sum = sum + i
-    i = i + 1
-sum
+    if i % 3 == 0 and i % 5 == 0 or i % 3 == 0 or i % 5 == 0 :
+        sum += i
+    i += 1
+print("Otvet", sum)
